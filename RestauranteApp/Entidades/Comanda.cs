1cdf2cf73d57e18c73e9c24ca50a6f21ef49befd
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-02-25 18:05:08 -0300

    Implementacao interface nas classes para usar abstracao de banco em csv

--- RestauranteApp/Entidades/Comanda.cs
+++ RestauranteApp/Entidades/Comanda.cs
@@ -1,15 +1,21 @@
-﻿using System;
+﻿using RestauranteApp.Interfaces;
+using System;
 
 namespace RestauranteApp.Entidades
 {
-    class Comanda
+    class Comanda : ParseToCsv
     {
         public int ComandaId { get; set; }
         public int MesaId { get; set; }
         public DateTime DataHoraEntrada { get; set; }
-        public DateTime DataHoraSaida { get; set; }
+        public DateTime? DataHoraSaida { get; set; }
         public float Valor { get; set; }
         public bool Paga { get; set; }
         public int QuantidadeClientes { get; set; }
+
+        public string Imprimir()
+        {
+            return $"";
+        }
     }
 }
--- RestauranteApp/Entidades/Pedido.cs
+++ RestauranteApp/Entidades/Pedido.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using RestauranteApp.Interfaces;
 
 namespace RestauranteApp.Entidades
 {
-    class Pedido
+    class Pedido : ParseToCsv
     {
         public int PedidoId { get; set; }
         public int ComandaId { get; set; }
         public int ProdutoId { get; set; }
         public int Status { get; set; }
         public int Quantidade { get; set; }
+
+        public string Imprimir()
+        {
+            return $"{ PedidoId },{ ComandaId },{ ProdutoId },{ Status },{ Quantidade }";
+        }
     }
 }
