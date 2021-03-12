ad27822a9b96d3a4c29e1e3a303dc2506f7614d0
Felipe Wesley <fe.wesleybasso@gmail.com>
2021-03-12 13:50:12 -0300

    Alteracao dos nomes de metodos

--- Restaurante.WebAPI/Controllers/ComandaController.cs
+++ Restaurante.WebAPI/Controllers/ComandaController.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Restaurante.Repositorio.Services.Comanda;
 using Restaurante.Repositorio.Services.Comanda.Models;
-using System.Threading.Tasks;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Restaurante.WebAPI.Controllers
 {
@@ -14,35 +12,32 @@ namespace Restaurante.WebAPI.Controllers
 
         private readonly IComandaService _service;
 
-        public ComandaController(IComandaService service)
-        {
-            _service = service;
-        }
+        public ComandaController(IComandaService service) => _service = service;
 
-        // GET api/<ComandaController>/5
+        // GET api/<ComandaController>/ObterComandaResumida/5
         [HttpGet("ObterComandaResumida/{id}")]
         public async Task<ComandaResumidaModel> ObterComandaResumida(int id)
         {
             return await _service.ObterComandaResumida(id);
         }
 
-        // GET api/<ComandaController>/5
+        // GET api/<ComandaController>/ObterComandaCompleta/5
         [HttpGet("ObterComandaCompleta/{id}")]
         public async Task<ComandaCompletaModel> ObterComandaCompleta(int id)
         {
             return await _service.ObterComandaCompleta(id);
         }
 
-        // POST api/<ComandaController>
-        [HttpPost("NovaComanda")]
-        public async Task RegistrarNovaComanda(ComandaFormularioModel comanda)
+        // POST api/<ComandaController>/Registrar
+        [HttpPost("Registrar")]
+        public async Task Registrar(ComandaFormularioModel comanda)
         {
             await _service.RegistrarComanda(comanda);
         }
 
-        // PUT api/<ComandaController>/5
+        // PUT api/<ComandaController>/Encerrar/5/true?
         [HttpPut("EncerrarComanda/{id}/{porcentagemGarcom?}")]
-        public async Task EncerrarComanda(int id, bool porcentagemGarcom = false)
+        public async Task Encerrar(int id, bool porcentagemGarcom = false)
         {
             await _service.EncerrarComanda(id, porcentagemGarcom);
         }
